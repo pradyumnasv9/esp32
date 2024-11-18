e3b9e48ba5b5175f79ba10be1d21bf91eba6ee4b
pradyumnsv9 <svpradyumna.1rn18ec118@gmail.com>
2024-11-18 17:05:36 +0530

    add links

--- ide/7474/codes/.pio/build/esp32doit-devkit-v1/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/USB.cpp.o: \
- /home/psv/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- /home/psv/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /home/psv/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
